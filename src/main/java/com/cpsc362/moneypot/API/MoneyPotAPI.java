6fea277e30faaab6ebb89742801ea5f541be6c4a
Nicholas Caro <nicholascaro@Nicholass-MacBook-Air-3.local>
2022-09-09 21:40:16 -0700

    added CrossOrigin annotatin to api endpoints

--- src/main/java/com/cpsc362/moneypot/API/MoneyPotAPI.java
+++ src/main/java/com/cpsc362/moneypot/API/MoneyPotAPI.java
@@ -3,10 +3,7 @@ package com.cpsc362.moneypot.API;
 
 import com.cpsc362.moneypot.Dao.models.Pot;
 import com.cpsc362.moneypot.Service.MoneyPot;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -14,11 +11,13 @@ import java.util.Map;
 public class MoneyPotAPI {
     MoneyPot moneyPot = new MoneyPot();
 
+    @CrossOrigin
     @RequestMapping(method = RequestMethod.DELETE, path = "/delete")
     public String deletePot (@RequestBody Map<String, Object> requestBodyParam) {
         return moneyPot.deleteMoneyPot(requestBodyParam);
     }
 
+    @CrossOrigin
     @RequestMapping(method = RequestMethod.POST, path = "/create")
     public String createPot (@RequestBody Pot potModel) {
         return moneyPot.addMoneyPot(potModel);
